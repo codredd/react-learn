372b80c7826eb1c77cd2367b97c0a6f46ccc6172
John Mathew <codredd@outlook.com>
2024-03-07 16:59:47 +0100

    lip: interaction added to the square components

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -17,15 +17,33 @@ The "return" means that whatever comes after will be returned as a value to the 
 
 import './App.css';
 
+// "useState" is a hook that allows you to have state variables in functional components. 
+import { useState } from 'react';
+
+
 // We create the new component "Square" and import it into the "Board" component
 // As next step, we will update the "Square" component to accept a prop and display it
-function Square({ value }: { value: number }) { 
+function Square() {
   // A major difference between jsx and tsx is that props are "typed" in tsx. 
   //This means that the type of the prop is defined in the function signature
-  return <button className="square">{value}</button>;
+
+  const [value, setValue] = useState<string>();
+
+  // Next step is adding interaction to the "Square" component. We will add a click event to the button and make the code remember that it was clicked
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
-export default function Board(){
+export default function Board() {
   /* 
   
   return <button className="square">X</button><button className="square">X</button>; 
@@ -35,26 +53,27 @@ export default function Board(){
    
    */
 
-   
+
   // Button elements were replaced by : <Square /> components by github copilot
   // The value prop was added to the Square component using github copilot. Automatically added values from 1 to 9
+  // props were removed as we move on to the interactive part of the tutorial
   return (
     <>
       <div className='board-row'>
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='board-row'>
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='board-row'>
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
-      </div>          
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   )
 }
